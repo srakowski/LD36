507c79b3b3a01c1ac605a3d03150e0145ad55988
Shawn Rakowski <srakowski@live.com>
2016-08-26 19:43:08 -0500

    boilerplate commit

---
+++ src/LD36/LD36Game.cs
@@ -0,0 +1,14 @@
+﻿using Coldsteel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LD36
+{
+	class LD36Game : Game
+	{
+		public override void Initialize()
+		{
+		}
+	}
+}
